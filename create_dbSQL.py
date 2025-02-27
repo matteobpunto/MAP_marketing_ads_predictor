c866a04f1f71d3404d1cf44fccf41a88b8f9b8f6
AnnaOccoffer <annaoccoffer@gmail.com>
2025-02-27 12:10:58 +0100

    update db mongodb and sql.py
    translate in english all the print and modify with clear_csv

--- create_dbSQL.py
+++ create_dbSQL.py
@@ -1,9 +1,9 @@
-#code for the database creation using SQL from pyhton
+# Code for the database creation using SQL from Python
 
 import mysql.connector
 import csv
 
-#Connessione al server MySQL
+# Connect to the MySQL server
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -12,7 +12,7 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
-# create db SQL
+# Create SQL database
 cursor.execute("CREATE DATABASE IF NOT EXISTS db_marketing_adv")
 
 cursor.execute("USE db_marketing_adv")
@@ -25,37 +25,38 @@ CREATE TABLE IF NOT EXISTS marketing_adv (
     sales FLOAT
 )
 """)
-# #...........................................
-#create table on SQL
-with open('Advertising_modified.csv', "r", encoding='utf-8') as f:
+
+# ...........................................
+# Create table in SQL and insert data
+with open('Advertising_clear.csv', "r", encoding='utf-8') as f:
     reader = csv.DictReader(f)
-    lista_marketing = []  # Lista per contenere i dati puliti
+    marketing_list = []  # List to store cleaned data
 
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
-        database = "db_marketing_adv"
+        database="db_marketing_adv"
     )
 
     cursor = conn.cursor()
 
     for row in reader:
         try:
-            # Convertire le colonne in float e pulire i dati
+            # Convert columns to float and clean data
             row['TV'] = float(row['TV']) if row['TV'].strip() else None
             row['radio'] = float(row['radio']) if row['radio'].strip() else None
             row['newspaper'] = float(row['newspaper']) if row['newspaper'].strip() else None
             row['sales'] = float(row['sales']) if row['sales'].strip() else None
 
-            lista_marketing.append(row)  # Aggiungere il dizionario pulito alla lista
+            marketing_list.append(row)  # Add cleaned dictionary to the list
             cursor.execute(f"""INSERT INTO `marketing_adv`(`tv`, `radio`, `newspaper`, `sales`) VALUES
-                ('{row['TV']}','{row['radio']}','{row['newspaper']}','{row['sales']}')""")
+                ('{row['TV']}', '{row['radio']}', '{row['newspaper']}', '{row['sales']}')""")
 
         except ValueError as e:
-            print(f"Riga ignorata a causa di errore di conversione: {row}. Errore: {e}")
-    print("Dati inseriti con successo")
+            print(f"Row skipped due to conversion error: {row}. Error: {e}")
+
+    print("Data successfully inserted")
 
 conn.commit()
 conn.close()
-
