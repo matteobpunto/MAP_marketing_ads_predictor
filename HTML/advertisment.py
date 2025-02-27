dcc1beb931357b3ea015a3e39a7f6bfba6b24b24
Saygex2004 <borodasc@gmail.com>
2025-02-27 11:02:21 +0100

    BUONGIORNISSIMO

--- HTML/advertisment.py
+++ HTML/advertisment.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
-# Dati
-marketing_data = [
-    {'tv': 200.5, 'radio': 39.3, 'newspaper': 69.8, 'sales': 22.1},
-    {'tv': 150.2, 'radio': 45.1, 'newspaper': 80.5, 'sales': 18.3},
-    {'tv': 300.7, 'radio': 20.5, 'newspaper': 50.4, 'sales': 25.6}
-]
+# Данные для отображения в таблице
+
+with open('Advertising_modified.csv', "r", encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    marketing_data = []  # Lista per contenere i dati puliti
+
+    for row in reader:
+        try:
+            # Convertire le colonne in float e pulire i dati
+            row['TV'] = float(row['TV']) if row['TV'].strip() else None
+            row['radio'] = float(row['radio']) if row['radio'].strip() else None
+            row['newspaper'] = float(row['newspaper']) if row['newspaper'].strip() else None
+            row['sales'] = float(row['sales']) if row['sales'].strip() else None
+
+            marketing_data.append(row)  # Aggiungere il dizionario pulito alla lista
+            #cursor.execute(f"""INSERT INTO `marketing_adv`(`tv`, `radio`, `newspaper`, `sales`) VALUES
+                #('{row['TV']}','{row['radio']}','{row['newspaper']}','{row['sales']}')""")
+
+        except ValueError as e:
+            print(f"Riga ignorata a causa di errore di conversione: {row}. Errore: {e}")
+    print("Dati inseriti con successo")
+
+#conn.commit()
+#conn.close()
+
 
 @app.route('/')
 def index():
