fbfc612ab0a09148ff2ed0c3f5044ce5ea14686c
alimonti99 <alice.monti.am@gmail.com>
2025-02-28 14:54:16 +0100

    create inserimento_target.py
    Insertion of data into the target table of database2

---
+++ database2/inserimento_target.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+# Connect to the database
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="marketing_adv"
+)
+
+cursor = conn.cursor()
+
+# SQL query to insert data into the 'target' table
+query = "INSERT INTO target (age_range) VALUES (%s)"
+values = [
+    ('under 18',),
+    ('18 - 25',),
+    ('25 - 40',),
+    ('40 - 60',),
+    ('over 60',)
+]
+
+
+cursor.executemany(query, values)
+
+# Commit the changes to the database
+conn.commit()
+
+print("Dati inseriti correttamente!")
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
