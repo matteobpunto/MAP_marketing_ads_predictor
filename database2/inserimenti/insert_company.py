1d7e4814ce15642d508151e6f0e9aed1e0914ec7
AnnaOccoffer <annaoccoffer@gmail.com>
2025-03-07 09:37:43 +0100

    Update insert_company.py
    translate in english

--- database2/inserimenti/insert_company.py
+++ database2/inserimenti/insert_company.py
@@ -10,28 +10,29 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
+# Select the database
 cursor.execute("USE marketing_adv")
 
+# Open the CSV file and read its contents
+with open('Italian_companies.csv', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    f.readline()  # Skip the header row
 
-with open('Aziende_Italiane.csv', encoding='utf-8') as f:
-    lettore = csv.reader(f, delimiter=',')
-    f.readline()
-
-    lista_prova = []
-    for riga in lettore:
-
-        query = f"INSERT INTO company (name, office, n_employees) VALUES ('{riga[0]}','{riga[1]}', {riga[2]})"
+    query_list = []
+    for row in reader:
+        query = f"INSERT INTO company (name, office, n_employees) VALUES ('{row[0]}','{row[1]}', {row[2]})"
         print(query)
 
         cursor.execute(query)
 
-        lista_prova.append(query)
+        query_list.append(query)
 
-
+# Commit the changes to the database
 conn.commit()
 
 print("Data inserted successfully!")
-print(lista_prova)
+print(query_list)
 
+# Close the connection
 cursor.close()
 conn.close()
