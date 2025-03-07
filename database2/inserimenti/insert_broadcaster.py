b3c82c0241c7951b845d067ef440662055f031a5
AnnaOccoffer <annaoccoffer@gmail.com>
2025-03-07 09:47:04 +0100

    Update insert_broadcaster.py
    translate in english

--- database2/inserimenti/insert_broadcaster.py
+++ database2/inserimenti/insert_broadcaster.py
@@ -10,28 +10,29 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
+# Select the database
 cursor.execute("USE marketing_adv")
 
+# Open the CSV file and read its contents
+with open('Unique_Italian_Television_Networks.csv', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    f.readline()  # Skip the header row
 
-with open('Reti_Televisive_Italiane_Uniche.csv', encoding='utf-8') as f:
-    lettore = csv.reader(f, delimiter=',')
-    f.readline()
-
-    lista_prova = []
-    for riga in lettore:
-
-        query = f"INSERT INTO broadcaster (name, network) VALUES ('{riga[0]}','{riga[1]}')"
+    query_list = []
+    for row in reader:
+        query = f"INSERT INTO broadcaster (name, network) VALUES ('{row[0]}','{row[1]}')"
         print(query)
 
         cursor.execute(query)
 
-        lista_prova.append(query)
+        query_list.append(query)
 
-
+# Commit the changes to the database
 conn.commit()
 
 print("Data inserted successfully!")
-print(lista_prova)
+print(query_list)
 
+# Close the connection
 cursor.close()
 conn.close()
